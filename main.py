7d3cd29b1a493a9322ac941311c389b942123ed0
mxilia <ravintkosol@gmail.com>
2025-01-19 21:11:04 +0700

    movement

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ SCR_HEIGHT = 600
 run = True
 
 screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+clock = pygame.time.Clock()
 
-plr = Player()
+plr = Player(SCR_WIDTH, SCR_HEIGHT)
 
 def checkEvent():
     for e in pygame.event.get():
@@ -19,6 +20,7 @@ def checkEvent():
     return
 
 def paint():
+    screen.fill((0, 0, 0))
     plr.draw(screen)
     pygame.display.update()
     return
@@ -26,10 +28,19 @@ def paint():
 def update():
     checkEvent()
     key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        plr.move(0, -plr.speed)
+    elif key[pygame.K_a]:
+        plr.move(-plr.speed, 0)
+    elif key[pygame.K_s]:
+        plr.move(0, plr.speed)
+    elif key[pygame.K_d]:
+        plr.move(plr.speed, 0)
     return
 
 if __name__ == "__main__":
     while run:
         update()
         paint()
+        clock.tick(60)
     pygame.quit()
--- player.py
+++ player.py
@@ -1,11 +1,22 @@
 import pygame
 
 class Player:
-    player = pygame.Rect((300, 300, 50 ,50))
-    color = (255, 0, 0)
+    width = 20
+    height = 20
+    speed = 4
+    color = (0, 255, 0)
 
-    def __init__(self):
-        pass
+    def __init__(self, SCR_WIDTH, SCR_HEIGHT):
+        self.SCR_WIDTH = SCR_WIDTH
+        self.SCR_HEIGHT = SCR_HEIGHT
+        self.player = pygame.Rect((SCR_WIDTH/2-self.width, SCR_HEIGHT/2-self.height, self.width, self.height))
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.player)
+
+    def move(self, dx, dy):
+        if self.player.x+dx<0 or self.player.x+self.width+dx>self.SCR_WIDTH:
+            return
+        if self.player.y+dy<0 or self.player.y+self.height+dy>self.SCR_HEIGHT:
+            return
+        self.player.move_ip(dx, dy)
